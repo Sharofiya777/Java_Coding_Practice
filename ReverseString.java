258a80e70afa36cccabc3a17bca24abbcef93429
Sharofiya P <167209762+Sharofiya777@users.noreply.github.com>
2026-08-27 12:00:04 +0530

    Add reverse string

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ReverseString{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        StringBuilder sb=new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
+        sc.close();
+    }
+}
