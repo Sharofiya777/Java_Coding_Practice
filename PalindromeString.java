5b243d9a3e4f9d0f92dfeb659e6374d0f76c0ba1
Sharofiya P <167209762+Sharofiya777@users.noreply.github.com>
2026-08-27 11:58:59 +0530

    Add Palindrome String

---
+++ PalindromeString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PalindromeString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+       boolean isPalindrome = true;
+       int left=0;
+       int right = str.length()-1;
+       while(left<right){
+        if(str.charAt(left) != str.charAt(right)){
+            isPalindrome = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+    if(isPalindrome){
+        System.out.println("Palindrome");
+    }
+    else{
+        System.out.println("Not palindrome");
+    }
+    sc.close();
+    }
+}
